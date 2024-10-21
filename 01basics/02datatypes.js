cb00b01bf56a2dfcd9686d959e973e094442460c
DARSHANDEEPHC <darshandeepak18@gmail.com>
2024-10-21 17:58:12 +0000

    datatypes

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as  newer version
+// alert(3 + 3) we are not supposed to use this alert in node js. we can use alert(3 + 3) method only in the browser console
+
+let name="Darshan"
+let age=26
+let isLoggedIn =  true //Boolean type of data type
+
+console.log("Deepak");
+
+
+/* Primitive datatypes
+// number=> 2  to the power of 53
+// String =>""
+// bigint => used in larger number
+// bolean => true of false
+// null => standalone value
+// undefined => means value is not defined
+// symbol => to define the unique values
+*/
+
+// Objective type
+console.log(typeof "Darshan")
+console.log(typeof 26);
+console.log(typeof null); // we get o/p as object
+
+console.log(typeof undefined); // we get o/p as undefined
+
+
+
+
