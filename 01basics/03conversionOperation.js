a422395bacb6add4892dffab505c76b74f524060
DARSHANDEEPHC <darshandeepak18@gmail.com>
2024-10-25 08:34:56 +0000

    conversion of 1 datatype into another datatype

---
+++ 01basics/03conversionOperation.js
@@ -0,0 +1,30 @@
+//Converting Number to String 
+
+let number =1 //number
+console.log(number); //print the number
+console.log(typeof number); //print the type of datatype
+console.log(number.length); //print the length of the value
+
+//Converting
+let num=String(1+13) //convrting number into string ny using String datatype
+console.log(num);
+console.log(num.length);
+console.log(typeof num);
+
+
+let str="darshan"
+console.log(str);
+console.log(typeof str);
+console.log(str.length);
+
+let m=true
+let n=Boolean(12)
+console.log(m);
+console.log(n);
+console.log(typeof n,m);
+
+
+
+
+
+
