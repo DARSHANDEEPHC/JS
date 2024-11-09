91a05516933f7305112bd24356cbbcd666887747
DARSHANDEEPHC <darshandeepak18@gmail.com>
2024-11-09 06:47:18 +0000

    Number and Math Operations in js

--- 01basics/03conversionOperation.js
+++ 01basics/03conversionOperation.js
@@ -25,17 +25,52 @@ console.log(typeof n,m);
 
 //using toString()
  val =(5).toString();
+ valu=true.toString();
+ console.log(typeof valu);
  console.log(val);
  console.log(typeof val);
- 
+
+//Boolean to string
+val=String(true);
+console.log(typeof val);
+
+//Date to String
+val= String(new Date());
+console.log(val);
+console.log(typeof val);
+
+//Array to String
+val=String([1,2,3,4]);
+console.log(val);
+console.log(typeof val);
+
 //string to Number
 
 val=Number("2");
 console.log(val.toFixed(3)); //toFixed() method is used to add the decimal after a number //toFixed() can't used with val=2;
 console.log(typeof val);
 
+//parseInt is a another method of number. like we use toString method in String
+val=parseInt("5");
+console.log(typeof val);
 
- 
+//Type Coehesion
+
+const val1=1;
+const val2=2;
+const val3="3"
+const sum=val1+val2+val3;
+console.log(sum);
+console.log(typeof sum);
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ 01basics/04Numbers and Math Objects.js
@@ -0,0 +1,52 @@
+//Numbers and Math Object
+const num1=100;
+const num2=50;
+
+let val1;
+let val2;
+let val3;
+let val4;
+let val5;
+
+val1=num1+num2;
+val2=num1-num2;
+val3=num1*num2;
+val4=num1/num2;
+val5=num1%num2;
+
+console.log(val1);
+console.log(val2);
+console.log(val3);
+console.log(val4);
+console.log(val5);
+
+//Math objects are the inbuilt javascript Objects
+val=Math.PI; //returns the PI value
+console.log(val);
+
+val=Math.round(2.9); //The Math.round() method rounds a number to the nearest integer. 
+console.log(val);
+
+val=Math.ceil(2.1); //The Math.ceil() method rounds a number rounded UP to the nearest integer.
+console.log(val);
+
+val=Math.floor(2.2); //The Math.floor() method rounds a number DOWN to the nearest integer.
+console.log(val);
+
+val=Math.sqrt(64); //The Math.sqrt() method returns the square root of a number.
+console.log(val);
+
+val=Math.abs(-3); //The Math.abs() method returns the absolute value of a number.
+console.log(val);
+
+val=Math.pow(1,4); //The Math.pow() method returns the value of x to the power of y (xy).
+console.log(val);
+
+val=Math.min(2,5,44,10); //The Math.min() method returns the number with the lowest value.
+console.log(val);
+
+val=Math.max(2,5,44,10); //The Math.max() method returns the number with the Highest value.
+console.log(val);
+
+
+
