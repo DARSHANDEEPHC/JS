ec7a884e82d7ed5592253f413132118f3ab66cf1
DARSHANDEEPHC <darshandeepak18@gmail.com>
2024-11-12 13:24:28 +0000

    Condition and comparision in JS

---
+++ 01basics/09Conditional and Comparision.js
@@ -0,0 +1,28 @@
+//Conditional & Comparision in JS
+// Assignment operator
+//equal to
+const id=100;
+if(id==100)
+{
+    console.log("true");
+    
+}
+else{
+    console.log("false");
+    
+}
+
+//Not equal to
+
+const id1=200;
+if(id1!=100)
+{
+    console.log("true");
+    
+}
+else{
+    console.log("false");
+    
+}
+
+//
