0bf4d7d0898712b57c61e494e93e8dba0b1db8be
DARSHANDEEPHC <darshandeepak18@gmail.com>
2024-11-14 05:36:34 +0000

    Function declaration and Function Expesssion in JS

--- 01basics/10Function declaration and Expression.js
+++ 01basics/10Function declaration and Expression.js
@@ -1,12 +1,12 @@
 //In function declaration "function" keyword is mandatory for declaration we can call the function n number of timess if we want.
 
 //function declaration
-function greet()
+function greet() //declaring the function.
 {
     console.log("hi");
     
 }
-greet()  //calling the function greet() to go get the o/p.we can call multiple times it will gave same o/p.
+greet()  //calling the function greet() to go get the o/p.we can call the function greet() multiple times it will gave same o/p.
 
 //Another method
 function greeting()
@@ -30,7 +30,7 @@ function greetings(firstName="Deepak")
 console.log(greetings());
 
 //function expression
-const mul=function() //First we are declaring the variabl "const" for that variable we are declaring the function().
+const mul=function() //assigning  the function to the variable.
 {
     return "How are you"
 }
@@ -43,3 +43,49 @@ const multiplication=function(a) //First we are declaring the variabl "const" fo
 
 }
 console.log(multiplication(5));
+
+
+
+const multi=function(a,b)
+{
+    return a*b;
+
+}
+console.log(multi(5,2));
+
+
+//IIFE-- Immediately Invokable Function Expression.
+
+(function()
+{
+    console.log("IIFE ran...");
+    
+})();
+
+//
+(function(name)
+{
+    console.log(`${name} ran...`);
+    
+})("Darshan");
+
+//Declaring the function inside the object
+
+const person={
+    getBirthday:function()
+    {
+        return "26 jan 1998";
+        
+    }
+}
+console.log(person.getBirthday());
+
+
+const person1={
+    getBirthday:function(day)
+    {
+        return `January ${day}`;
+        
+    },
+}
+console.log(person1.getBirthday(26));
