f6b7c575157ea98f8df90b190b558b2f8baf80dd
Darshan H C <darshandeepak18@gmail.com>
2024-11-25 06:37:43 +0000

    dom

--- 01basics/12DOM single element selectors.js
+++ 01basics/12DOM single element selectors.js
@@ -1,3 +1,4 @@
 // single element selector
 // document.getElementById 
 // selecting a single element from HTML and changing or updating that HTML element in JavaScript.
+const forms=document.getElementById("Action")
