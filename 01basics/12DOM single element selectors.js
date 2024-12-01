ad62a3f4481e742d15019f63ba37af06f61994a9
Darshan H C <darshandeepak18@gmail.com>
2024-12-01 10:09:25 +0000

    DOM Examination

--- 01basics/12DOM single element selectors.js
+++ 01basics/12DOM single element selectors.js
@@ -2,9 +2,28 @@
 // document.getElementById 
 // selecting a single element from HTML and changing or updating that HTML element in JavaScript.
 
+let val;  //Declaring one variable
+val=document;  //It shows the document in the console.
+val=document.all; //It shows all the html documents used in the html file.
+val=document.all[0]; //It will displays the 1st content of the document.
+val=document.all.length; //It will displays the length of the document.
+val=document.head;  ////It will displays the head of the document.
+val=document.body; //It will displays the body of the document.
+val=document.doctype;  //It will displays the type of the document.
+val=document.URL;
+val=document.domain;
+val=document.characterSet; //It shows which kind of charset is used like (UTF).
+val=document.forms;
+val=document.forms[0];
+val=document.table.id;
+val=document.links;
 
-console.log(document.getElementsByClassName("Sales-table"));
-console.log(document.getElementById("para"));
+
+
+console.log(val);
+
+// console.log(document.getElementsByClassName());
+// console.log(document.getElementById());
 
 //change style
 
